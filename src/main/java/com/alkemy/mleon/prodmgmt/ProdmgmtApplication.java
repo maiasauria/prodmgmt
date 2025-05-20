525c69ba74ca48c4ce4ddf712a204bc63b77a13b
maiasauria <maiasauria1@gmail.com>
2025-05-20 11:49:41 -0300

    Initial commit

---
+++ src/main/java/com/alkemy/mleon/prodmgmt/ProdmgmtApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.mleon.prodmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdmgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdmgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alkemy/mleon/prodmgmt/ProdmgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.mleon.prodmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdmgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
